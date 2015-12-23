92d404bff5b5ddd0243eb08be84def2f45e68e0c
Per Arneng <per.arneng@scalebit.com>
2015-12-24 00:37:50 +0100

    more logic

--- src/main/ts/apassgen.ts
+++ src/main/ts/apassgen.ts
@@ -1,43 +1,60 @@
 
 export class PasswordTool {
-        
+           
     charsLowerAlpha:Array<string> = new Array<string>();    
     charsUpperAlpha:Array<string> = new Array<string>();    
     charsNumerical:Array<string> = new Array<string>();    
     charsSpecial:Array<string> = new Array<string>();     
     
+    charClasses:Array<Array<string>> = new Array<Array<string>>();
+    
     constructor() {
         
         for (var i=97;i<123;i++) {
             this.charsLowerAlpha.push(String.fromCharCode(i))
-            this.charsUpperAlpha.push(String.fromCharCode(i-32))
-            
+            this.charsUpperAlpha.push(String.fromCharCode(i-32))   
         }
-        console.log(this.charsLowerAlpha)
-        console.log(this.charsUpperAlpha)
                 
         for (var i=48;i<58;i++) {
             this.charsNumerical.push(String.fromCharCode(i))
         }
-        console.log(this.charsNumerical)
-
+        
         for (var i=32;i<126;i++) {
             if (i>96 && i<123) continue;
             if (i>64 && i<91) continue;
             if (i>47 && i<58) continue;
             this.charsSpecial.push(String.fromCharCode(i))
         }
-        console.log(this.charsSpecial)
         
+        this.charClasses.push(this.charsLowerAlpha)
+        this.charClasses.push(this.charsUpperAlpha)
+        this.charClasses.push(this.charsSpecial)
+        this.charClasses.push(this.charsNumerical)
     }
         
     createSibling(password:string) : string {
         
         var length = password.length;
         
+        for (var c of password) {
+                       
+            var currentClass = this.charsLowerAlpha;
+          
+            for (var charClass of this.charClasses) {
+                if (charClass.indexOf(c) > -1) {
+                    currentClass = charClass;
+                }
+            }
+            
+            var newChar = currentClass[0]
+            
+            console.log(newChar)
+            console.log(c)
+        }
         
         return password;
     }
        
 }
+
 
--- target/apassgen.js
+++ target/apassgen.js
@@ -4,16 +4,14 @@ var PasswordTool = (function () {
         this.charsUpperAlpha = new Array();
         this.charsNumerical = new Array();
         this.charsSpecial = new Array();
+        this.charClasses = new Array();
         for (var i = 97; i < 123; i++) {
             this.charsLowerAlpha.push(String.fromCharCode(i));
             this.charsUpperAlpha.push(String.fromCharCode(i - 32));
         }
-        console.log(this.charsLowerAlpha);
-        console.log(this.charsUpperAlpha);
         for (var i = 48; i < 58; i++) {
             this.charsNumerical.push(String.fromCharCode(i));
         }
-        console.log(this.charsNumerical);
         for (var i = 32; i < 126; i++) {
             if (i > 96 && i < 123)
                 continue;
@@ -23,10 +21,26 @@ var PasswordTool = (function () {
                 continue;
             this.charsSpecial.push(String.fromCharCode(i));
         }
-        console.log(this.charsSpecial);
+        this.charClasses.push(this.charsLowerAlpha);
+        this.charClasses.push(this.charsUpperAlpha);
+        this.charClasses.push(this.charsSpecial);
+        this.charClasses.push(this.charsNumerical);
     }
     PasswordTool.prototype.createSibling = function (password) {
         var length = password.length;
+        for (var _i = 0; _i < password.length; _i++) {
+            var c = password[_i];
+            var currentClass = this.charsLowerAlpha;
+            for (var _a = 0, _b = this.charClasses; _a < _b.length; _a++) {
+                var charClass = _b[_a];
+                if (charClass.indexOf(c) > -1) {
+                    currentClass = charClass;
+                }
+            }
+            var newChar = currentClass[0];
+            console.log(newChar);
+            console.log(c);
+        }
         return password;
     };
     return PasswordTool;
